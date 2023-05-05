c364cc0291ea475083569fb0b88f72263f9e15b1
maki <mkr.fb-66@cameo.plala.or.jp>
2023-05-05 14:16:11 +0900

    first commit

---
+++ src/main/java/com/example/work7/Work7Application.java
@@ -0,0 +1,13 @@
+package com.example.work7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Work7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Work7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/work7/Work7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.work7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Work7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
